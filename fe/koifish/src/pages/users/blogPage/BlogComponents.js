0e63130fb66fdf1747496144ca85baf58d476085
Tran Duc Huy <2251120090@ut.edu.vn>
2024-10-11 23:24:50 +0700

    Huy commit

--- fe/koifish/src/pages/users/blogPage/BlogComponents.js
+++ fe/koifish/src/pages/users/blogPage/BlogComponents.js
@@ -1,16 +1,16 @@
-// src/pages/users/blogPage.js
-import { BlogComponents } from './index'; // Đảm bảo đường dẫn đúng
+// // src/pages/users/blogPage.js
+// import { BlogComponents } from './index'; // Đảm bảo đường dẫn đúng
 
-// Tạo một component BlogPage
-const BlogPage = () => {
-  return (
-    <div>
-      <BlogComponents.Post img="your_image_url" />
-      <BlogComponents.Posts />
-      <BlogComponents.SinglePost />
-    </div>
-  );
-};
+// // Tạo một component BlogPage
+// const BlogPage = () => {
+//   return (
+//     <div>
+//       <BlogComponents.Post img="your_image_url" />
+//       <BlogComponents.Posts />
+//       <BlogComponents.SinglePost />
+//     </div>
+//   );
+// };
 
-// Xuất khẩu BlogPage như là default export
-export default BlogPage;
+// // Xuất khẩu BlogPage như là default export
+// export default BlogPage;
--- fe/koifish/src/pages/users/blogPage/index.js
+++ fe/koifish/src/pages/users/blogPage/index.js
@@ -1,84 +1,84 @@
-// src/index.js
-import { Link } from "react-router-dom";
-import './style.scss';
+// // src/index.js
+// import { Link } from "react-router-dom";
+// import './style.scss';
 
-// Component Cha
-const BlogPage = () => {
-  // Định nghĩa Post component
-  const Post = ({ img }) => (
-    <div className="post">
-      <img className="postImg" src={img} alt="" />
-      <div className="postInfo">
-        <span className="postTitle">
-          <Link to="/post/abc" className="link">
-            KOI KOI FENG SHUI
-          </Link>
-        </span>
-        <hr />
-        <span className="postDate">1 hour ago</span>
-      </div>
-      <p className="postDesc">Lots of Koi fish</p>
-    </div>
-  );
+// // Component Cha
+// const BlogPage = () => {
+//   // Định nghĩa Post component
+//   const Post = ({ img }) => (
+//     <div className="post">
+//       <img className="postImg" src={img} alt="" />
+//       <div className="postInfo">
+//         <span className="postTitle">
+//           <Link to="/post/abc" className="link">
+//             KOI KOI FENG SHUI
+//           </Link>
+//         </span>
+//         <hr />
+//         <span className="postDate">1 hour ago</span>
+//       </div>
+//       <p className="postDesc">Lots of Koi fish</p>
+//     </div>
+//   );
 
-  // Định nghĩa Posts component
-  const Posts = () => (
-    <div className="posts">
-      <Post img="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTChRK-CfIjQUIkhOOJ9R-F5p7NuCssA8h5sA&s" />
-      <Post img="https://upload.wikimedia.org/wikipedia/commons/1/10/Ojiya_Nishikigoi_no_Sato_ac_%283%29.jpg" />
-      <Post img="https://nonbo.net.vn/wp-content/uploads/2019/03/Cac-loai-ca-koi-duoc-nuoi-nhieu-nhat.jpg" />
-      <Post img="https://ran.com.vn/wp-content/uploads/2021/02/Ca-Koi-Nhat-Ban-1.jpeg" />
-      <Post img="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQBow9xg4vKizGV6UoXHcHFdSeSLMMWBvSp_Q&s" />
-    </div>
-  );
+//   // Định nghĩa Posts component
+//   const Posts = () => (
+//     <div className="posts">
+//       <Post img="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTChRK-CfIjQUIkhOOJ9R-F5p7NuCssA8h5sA&s" />
+//       <Post img="https://upload.wikimedia.org/wikipedia/commons/1/10/Ojiya_Nishikigoi_no_Sato_ac_%283%29.jpg" />
+//       <Post img="https://nonbo.net.vn/wp-content/uploads/2019/03/Cac-loai-ca-koi-duoc-nuoi-nhieu-nhat.jpg" />
+//       <Post img="https://ran.com.vn/wp-content/uploads/2021/02/Ca-Koi-Nhat-Ban-1.jpeg" />
+//       <Post img="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQBow9xg4vKizGV6UoXHcHFdSeSLMMWBvSp_Q&s" />
+//     </div>
+//   );
 
-  // Định nghĩa SinglePost component
-  const SinglePost = () => (
-    <div className="singlePost">
-      <div className="singlePostWrapper">
-        <img
-          className="singlePostImg"
-          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT4Hv6UUYk3tTxS_w5zyT1Pzd4KsRA5r2bVoQ&s"
-          alt=""
-        />
-        <h1 className="singlePostTitle">
-          Lorem ipsum dolor
-          <div className="singlePostEdit">
-            <i className="singlePostIcon far fa-edit"></i>
-            <i className="singlePostIcon far fa-trash-alt"></i>
-          </div>
-        </h1>
-        <div className="singlePostInfo">
-          <span>
-            Author:
-            <b className="singlePostAuthor">
-              <Link className="link" to="/posts?username=Safak">
-                Safak
-              </Link>
-            </b>
-          </span>
-          <span>1 day ago</span>
-        </div>
-        <p className="singlePostDesc">
-          Koi koi koi koi koi koi koi koi Koi koi koi koi koi koi koi koi
-          Koi koi koi koi koi koi koi koi Koi koi koi koi koi koi koi koi
-          Koi koi koi koi koi koi koi koi Koi koi koi koi koi koi koi koi
-          Koi koi koi koi koi koi koi koi Koi koi koi koi koi koi koi koi
-          <br />
-          <br />
-          Koi koi koi koi koi koi koi koi Koi koi koi koi koi koi koi koi
-          Koi koi koi koi koi koi koi koi Koi koi koi koi koi koi koi koi
-          Koi koi koi koi koi koi koi koi Koi koi koi koi koi koi koi koi
-        </p>
-      </div>
-    </div>
-  );
+//   // Định nghĩa SinglePost component
+//   const SinglePost = () => (
+//     <div className="singlePost">
+//       <div className="singlePostWrapper">
+//         <img
+//           className="singlePostImg"
+//           src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT4Hv6UUYk3tTxS_w5zyT1Pzd4KsRA5r2bVoQ&s"
+//           alt=""
+//         />
+//         <h1 className="singlePostTitle">
+//           Lorem ipsum dolor
+//           <div className="singlePostEdit">
+//             <i className="singlePostIcon far fa-edit"></i>
+//             <i className="singlePostIcon far fa-trash-alt"></i>
+//           </div>
+//         </h1>
+//         <div className="singlePostInfo">
+//           <span>
+//             Author:
+//             <b className="singlePostAuthor">
+//               <Link className="link" to="/posts?username=Safak">
+//                 Safak
+//               </Link>
+//             </b>
+//           </span>
+//           <span>1 day ago</span>
+//         </div>
+//         <p className="singlePostDesc">
+//           Koi koi koi koi koi koi koi koi Koi koi koi koi koi koi koi koi
+//           Koi koi koi koi koi koi koi koi Koi koi koi koi koi koi koi koi
+//           Koi koi koi koi koi koi koi koi Koi koi koi koi koi koi koi koi
+//           Koi koi koi koi koi koi koi koi Koi koi koi koi koi koi koi koi
+//           <br />
+//           <br />
+//           Koi koi koi koi koi koi koi koi Koi koi koi koi koi koi koi koi
+//           Koi koi koi koi koi koi koi koi Koi koi koi koi koi koi koi koi
+//           Koi koi koi koi koi koi koi koi Koi koi koi koi koi koi koi koi
+//         </p>
+//       </div>
+//     </div>
+//   );
 
-  return (
-    <div>
-      <h1>Blog Page</h1>
-      <Posts />
-      <SinglePost />
-    </div>
-  );
-};
+//   return (
+//     <div>
+//       <h1>Blog Page</h1>
+//       <Posts />
+//       <SinglePost />
+//     </div>
+//   );
+// };
