5c77a13b013b592ddce7094b80c2df2f9abb58c0
HyHeHuoc <2251120157@ut.edu.vn>
2024-10-14 21:29:53 +0700

    update

--- fe/koifish/src/pages/users/loginPage/index.js
+++ fe/koifish/src/pages/users/loginPage/index.js
@@ -1,11 +1,28 @@
 import { memo } from 'react';
+import './style.scss'; // Import SCSS file
 
 const Login = () => {
-    return (
-        <>
-            <h1>Đăng nhập - Đăng ký</h1>
-        </>
-    );
+  return (
+    <div className="login-container">
+      <h1>Đăng nhập - Đăng ký</h1>
+      <form className="login-form">
+        <div className="form-group">
+          <label htmlFor="email">Email</label>
+          <input type="email" id="email" name="email" placeholder="Enter your email" required />
+        </div>
+        <div className="form-group">
+          <label htmlFor="password">Password</label>
+          <input type="password" id="password" name="password" placeholder="Enter your password" required />
+        </div>
+        <div className="form-group">
+          <button type="submit" className="btn-login">Login</button>
+        </div>
+        <div className="form-group">
+          <button className="forgot-password" onClick={() => alert('Handle forgot password')}>Forgot Password?</button>
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default memo(Login);
