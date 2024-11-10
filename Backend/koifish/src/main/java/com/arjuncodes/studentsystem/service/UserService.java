2ada36cde01a471392d6ea198a05fd266ffdba97
VTruong27012004 <2251120257@ut.edu.vn>
2024-11-10 23:08:10 +0700

    update trangcanhan

---
+++ Backend/koifish/src/main/java/com/arjuncodes/studentsystem/service/UserService.java
@@ -0,0 +1,10 @@
+package com.arjuncodes.studentsystem.service;
+
+import com.arjuncodes.studentsystem.model.User;
+
+import java.util.List;
+
+public interface UserService {
+    public User saveUser(User user);
+    public List<User> getAllUsers();
+}
